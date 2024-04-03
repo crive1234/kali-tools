53c38c96e0937e44d61470649555c4064c3c86ad
crive1234 <73899168+crive1234@users.noreply.github.com>
2024-04-03 17:24:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <thread>
 #include <chrono>
 
+using namespace std;
+
 // variabili
 char a;
 char caratteri_menu[] = {'1','2','3','0'};
@@ -13,11 +15,10 @@ int i;
 int out_err;
 
 // funzioni
-void cls();
 void menu(int i);
 void in();
 void villain();
-void display_invalid_input();
+void switch_default_invalid();
 void controllo_last_string_input(char controllo);
 
 
@@ -28,6 +29,7 @@ int main() {
     scrollok(stdscr, TRUE);
 
     menu(0);
+
     do {
         in();
         switch (a) {
@@ -35,23 +37,19 @@ int main() {
             case '2': printw("work in progress\n"); break;
             case '3': printw("work in progress\n"); break;
             case '0': endwin(); break;
-            default: display_invalid_input(); break;
+            default: switch_default_invalid(); break;
         }
     } while(a != '0');
 
     return 0;
 }
 
-void cls() {
-    clear();
-}
-
 void menu(int i) {
     switch(i) {
-        case 0: printw("Menu inizio\n1] villain\n2] work in progress\n3] work in progress\n0] Exit\n"); break;
-        case 1: printw("Menu villain\n"); break;
-        case 2: printw("Menu 2\n"); break;
-        case 3: printw("Menu 3\n"); break;
+        case 0: printw("Menu inizio\n\n1] villain\n2] work in progress\n3] work in progress\n0] Exit\n"); break;
+        case 1: printw("Menu villain\n\n1] villain\n2] work in progress\n3] work in progress\n0] Exit\n"); break;
+        case 2: printw("Menu 2\n\n1] villain\n2] work in progress\n3] work in progress\n0] Exit\n"); break;
+        case 3: printw("Menu 3\n\n1] villain\n2] work in progress\n3] work in progress\n0] Exit\n"); break;
     }
 }
 
@@ -67,10 +65,10 @@ void in() {
 void villain() {
     clear();
     menu(1);
-    getch();
 }
 
-void display_invalid_input() {
+void switch_default_invalid() {
+
     for(i = 0; i < sizeof(caratteri_menu); i++) {
         if(a != caratteri_menu[i]) {
             out_err++;
@@ -78,21 +76,21 @@ void display_invalid_input() {
             out_err = 0;
         }
     }
+
     if(out_err == 4) {
         printw("Input non valido\n");
-        // Avvia un thread che stampa un carattere ogni 5 secondi
-        std::thread contr_input(controllo_last_string_input, a);
+        // Avvia thread
+        thread contr_input(controllo_last_string_input, a);
         contr_input.detach(); // Rilascia il thread, permettendogli di continuare in background
     }
+
 }
 
 void controllo_last_string_input(char controllo) {
-    //while(true) {
-        std::this_thread::sleep_for(std::chrono::seconds(2)); // Attendi 5 secondi
-        //printw("%c\n", controllo); // Stampa il carattere
-        clear();
-        menu(0);
-        refresh(); // Aggiorna lo schermo
-        
-    //}
+
+    this_thread::sleep_for(chrono::seconds(2)); // Attendi 5 secondi
+    clear();
+    menu(0);
+    refresh();
+    out_err = 0;
 }
